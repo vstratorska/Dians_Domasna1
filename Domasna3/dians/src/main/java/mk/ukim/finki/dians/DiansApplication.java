19d7e31b1b2b8692cd8a68255a9782e3ff544ace
Viktorija Stratorska <viktorija.stratorska@gmail.com>
2023-12-14 18:52:31 +0100

    First commit on this branch

---
+++ Domasna3/dians/src/main/java/mk/ukim/finki/dians/DiansApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiansApplication.class, args);
+    }
+
+}
